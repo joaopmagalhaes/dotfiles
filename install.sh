fc3bf5d99456a293f3836bbd97d8203471e64107
João Magalhães <joao.magalhaes@kununu.com>
2020-06-04 10:03:54 +0100

    Add spotify, postman and vscode

--- install.sh
+++ install.sh
@@ -10,14 +10,21 @@ sudo add-apt-repository ppa:gnome-terminator
 # Add GIMP
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp
 
+# Add Spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
 # Update
 sudo apt update
 
 ## Install all packages
-sudo apt install build-essential slack openvpn network-manager-openvpn network-manager-openvpn-gnome gconf2 gimp google-chrome-stable file fonts-powerline git htop iftop mysql-workbench npm python-setuptools terminator unrar zsh parcellite --assume-yes
+sudo apt install build-essential slack openvpn snapd network-manager-openvpn spotify-client code network-manager-openvpn-gnome gconf2 gimp google-chrome-stable file fonts-powerline git htop iftop mysql-workbench npm python-setuptools terminator unrar zsh parcellite --assume-yes
 
 # Install deb dependencies
 sudo apt install -f --assume-yes
+
+# Install Postman
+sudo snap install postman
 
 # Install oh-my-zshell
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
